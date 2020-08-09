fcec2a8891f71562cd53b919d387a322da2bc27d
polopalayyy <51782822+polopalayyy@users.noreply.github.com>
2020-08-10 00:23:53 +0700

    bang

--- js/site.js
+++ js/site.js
@@ -10,49 +10,75 @@ const config = {
 };
 const database = new Database(config, false);
 async function getData() {
-    const data = await database.read("/posts/data/0");
+    const data = await database.read("/posts/data");
+    const user = await database.auth.currentUser;
+    if (data.length > 0) {
+        setSrc(data[0]);
+        fillDataToListPost(data, 6);
+    }
+    if (user == null) {
+        setLogin();
+    } else {
+        setLogout();
+    }
+}
+
+async function search(event) {
+    event.preventDefault();
+    console.log($("#keyword").val());
+    const data = await database.read("/posts/data");
+    const listRs = data.filter(element => {
+        return element.title.includes($("#keyword").val());
+    });
+    if (data.length > 0) {
+        fillDataToListPost(listRs, 100000);
+    }
+}
+
+async function login(event) {
+    event.preventDefault();
+    const result = await database.login($("#account").val(), $("#password").val());
+    if (result.rs) {
+        $("#error").html("");
+        $("#modelLogin").modal("toggle");
+        setLogout();
+    } else {
+        $("#error").html(result.mss);
+    }
+}
+
+function setLogin() {
+    $("#userManager").text("Đăng nhập");
+    $("#userManager").attr("data-target", "#modelLogin");
+}
+function setLogout() {
+    $("#userManager").text("Đăng xuất");
+    $("#linkToList").text("Danh sách");
+    $("#linkToList").attr("href", "/post/list");
+    $("#userManager").click(function () {
+        database.logout();
+        $("#userManager").text("Đăng nhập");
+        $("#linkToList").text("");
+        $("#userManager").attr("data-target", "#modelLogin");
+    });
+}
+function setSrc(data) {
     $("#title").text(data.title);
     $("#image").attr("src", data.image);
     $("#date").text(data.date);
     $("#description").text(data.description);
     $("#content").html(data.content);
-    $("#userManager").empty();
-    var user = await database.auth.currentUser;
-    if (user == null) {
-        $("#userManager").append($("<a>", {
-            class: "nav-link",
-            css: { "cursor": "pointer" },
-            "data-toggle": "modal",
-            "data-target": "#modelLogin",
-            text: "Đăng nhập"
-        }));
-    } else {
-        $("#userManager").append($("<a>", {
-            class: "nav-link",
-            css: { "cursor": "pointer" },
-            text: "Đăng xuất",
-            click: function () {
-                $("#userManager").empty();
-                database.logout();
-                $("#userManager").append($("<a>", {
-                    class: "nav-link",
-                    css: { "cursor": "pointer" },
-                    "data-toggle": "modal",
-                    "data-target": "#modelLogin",
-                    text: "Đăng nhập"
-                }));
-            }
-        }));
-    }
 }
-async function login(event) {
-    event.preventDefault();
-    const result = await database.login($('#account').val(), $('#password').val());
-    console.log(result);
-    if (result.rs) {
-        window.location.href = "/post/list";
-    } else {
-        $("#error").html(result.mss);
+function fillDataToListPost(data, limit) {
+    $("#otherPost").empty();
+    for (let i = 1; i < data.length && i < limit; i++) {
+        $("#otherPost").append($("<a>", {
+            text: data[i].title,
+            href: "#",
+            click: function () {
+                setSrc(data[i]);
+            }
+        }))
     }
 }
 getData();
